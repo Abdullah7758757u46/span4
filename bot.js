8d496fe19064fa81008b14722d14d6baf2a01f73
Abdullah7758757u46 <46163642+Abdullah7758757u46@users.noreply.github.com>
2018-12-31 01:21:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-console.log("Scrpit By Dream");
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("528319778213986304")
-setInterval(function() {
-channel.send(`سوف اموت`);
-}, 10 )
-})
+client.on('message',async message => {
+  if(message.content.startsWith(prefix + "setVoice")) {
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+  message.channel.send('✅| **تم عمل الروم بنجاح**');
+  message.guild.createChannel(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+    console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(function() {
+      c.setName(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]`)
+    },1000);
+  });
+  }
+});
+
+
+
+
 
 client.login('NTI4MzQ4NTM4Mzg0Njc4OTI2.DwjNZg.thpwFtPNbkDkpX22vLGA4qgGmL0');
